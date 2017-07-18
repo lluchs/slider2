a0cb295df366c6d80fe432586aa20dda1bf4b138
Lukas Werling <lukas.werling@gmail.com>
2017-07-18 17:03:11 +0200

    Refactor SlidingControl into separate component

--- src/slider.js
+++ src/slider.js
@@ -1,17 +1,49 @@
 import { h, render, Component } from 'preact'
 
+import {initDevTools} from 'preact/devtools/devtools'
+initDevTools()
+
+// Fancy image slider.
+// Props:
+//  - src: URI to image with {} as placeholder
+//  - labels: Array of labels with {name, title} objects. title is shown, name is inserted in src.
 class Slider extends Component {
+	constructor() {
+		super()
+		this.setCurrent = this.setCurrent.bind(this)
+
+		this.state.current = 0
+	}
+
+	setCurrent(index) {
+		this.setState({current: index})
+	}
+
+	render({src, labels}, {current, offsets, maxLabelWidth, dragging, dragOffset}) {
+		let {name, title} = labels[current]
+		let img = src.replace('{}', name)
+		return (
+			<div class="slider">
+				<img class="slider-image" src={img} alt={title} />
+				<SlidingControl labels={labels} current={current} onChange={this.setCurrent} />
+			</div>
+		)
+	}
+}
+
+// Provides a sliding selection control.
+// Props:
+//  - el: Element which can be dragged
+//  - labels: Array of labels with {name, title}
+//  - current: Index of currently active label
+//  - onChange(i): Function called when current changes to i
+class SlidingControl extends Component {
 	constructor() {
 		super()
 		this.dragging = this.dragging.bind(this)
 		this.stopDrag = this.stopDrag.bind(this)
 
-		this.state.current = 0
 		this.state.offsets = []
-	}
-
-	setCurrent(index) {
-		this.setState({current: index})
 	}
 
 	updateOffsets() {
@@ -26,7 +58,7 @@ class Slider extends Component {
 		document.body.addEventListener('pointermove', this.dragging)
 		document.body.addEventListener('pointerup', this.stopDrag)
 		document.body.addEventListener('pointercancel', this.stopDrag)
-		this.setState({dragging: true, dragOffset: this.state.offsets[this.state.current]})
+		this.setState({dragging: true, dragOffset: this.state.offsets[this.props.current]})
 		this.dragOrigin = event.clientX
 		event.preventDefault()
 	}
@@ -36,12 +68,13 @@ class Slider extends Component {
 		let dragOffset = this.state.dragOffset - this.dragOrigin + event.clientX
 		this.dragOrigin = event.clientX
 		// Update current image by comparing offsets of neighboring labels.
-		let {current, offsets} = this.state
+		let {offsets} = this.state
+		let {current, onChange} = this.props
 		if (current < offsets.length - 1 && dragOffset < (offsets[current] + offsets[current+1]) / 2)
-			current += 1;
+			onChange(current + 1)
 		else if (current > 0 && dragOffset > (offsets[current-1] + offsets[current]) / 2)
-			current -= 1;
-		this.setState({dragOffset, current})
+			onChange(current - 1)
+		this.setState({dragOffset})
 	}
 
 	stopDrag() {
@@ -59,22 +92,17 @@ class Slider extends Component {
 		this.stopDrag()
 	}
 
-	render({src, labels}, {current, offsets, maxLabelWidth, dragging, dragOffset}) {
+	render({labels, current, onChange}, {offsets, maxLabelWidth, dragging, dragOffset}) {
 		let {name, title} = labels[current]
-		let img = src.replace('{}', name)
 		let offset = dragging ? dragOffset : offsets[current]
 		return (
-			<div class="slider">
-				<img class="slider-image" src={img} alt={title} />
-
-				<div class="slider-wrapper" onPointerDown={e => this.startDrag(e)}>
-					<div class="marker" style={`width: ${maxLabelWidth}px`} ref={el => this.marker = el}></div>
-					<ol class={`slider-controls ${dragging ? '-dragging' : ''}`} style={`left: ${offset}px`} ref={el => this.controls = el}>
-						{labels.map(({title}, index) =>
-							<li class={current == index ? 'current' : ''} onClick={() => this.setCurrent(index)}>{title}</li>
-						)}
-					</ol>
-				</div>
+			<div class="slider-wrapper" onPointerDown={e => this.startDrag(e)}>
+				<div class="marker" style={`width: ${maxLabelWidth}px`} ref={el => this.marker = el}></div>
+				<ol class={`slider-controls ${dragging ? '-dragging' : ''}`} style={`left: ${offset}px`} ref={el => this.controls = el}>
+					{labels.map(({title}, index) =>
+						<li class={current == index ? 'current' : ''} onClick={() => onChange(index)}>{title}</li>
+					)}
+				</ol>
 			</div>
 		)
 	}
